8ef03b660f9607587c21c07178fcdafc7a41d829
golap-raj <golapraj47@gmail.com>
2025-03-12 02:24:38 +0600

    resolve

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,31 @@
+// API lode kortechi
 
-// API lode kortechi 
+function loadCatagori() {
+  //  1 --  akhon amra prothome fetch korbo
 
-function loadCatagori (){
-//  1 --  akhon amra prothome fetch korbo
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    // 2 -- convert json promised
 
-fetch ("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    // 3 -- send data to display
 
-// 2 -- convert json promised
-
-.then((res) => res.json())
-// 3 -- send data to display
-
-.then((data) => displayCatagori(data.categories) )
+    .then((data) => displayCatagori(data.categories));
 }
 
-function displayCatagori (categories){
-const catagoriContainar = document.getElementById("catagory-containar");
+function displayCatagori(categories) {
+  const catagoriContainar = document.getElementById("catagory-containar");
 
-for(let cat of categories){
-    // console.log(cat)
+  for (let cat of categories) {
+    console.log(cat)
 
     const creatDiv = document.createElement("div");
-creatDiv.innerHTML = `
-<button class="btn hover:bg-[#FF1F3D] hover:text-white">${cat.categories}</button>
+    creatDiv.innerHTML = `
+<button class="btn hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
 
 
-`// append child korlam 
-catagoriContainar.appendChild(creatDiv)
+`; // append child korlam
+    catagoriContainar.appendChild(creatDiv);
+  }
 }
 
-
-
-}
-
-loadCatagori()
+loadCatagori();
